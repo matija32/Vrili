249945b877730933d5edd3ff6cb1aa57a7334a99
Matija Lukic <matija32@gmail.com>
2017-02-19 20:33:38 +0100

    connected the button with the action to add activities

--- Vrili/Vrili.Droid/Resources/Resource.designer.cs
+++ Vrili/Vrili.Droid/Resources/Resource.designer.cs
@@ -187,8 +187,14 @@ namespace Vrili.Droid
 			// aapt resource value: 0x7f050002
 			public const int MvxBindingTagUnique = 2131034114;
 			
+			// aapt resource value: 0x7f050005
+			public const int activitiesListView = 2131034117;
+			
 			// aapt resource value: 0x7f050003
-			public const int addActivityButton = 2131034115;
+			public const int activityNameTextView = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int addActivityButton = 2131034116;
 			
 			// aapt resource value: 0x7f050000
 			public const int item_touch_helper_previous_elevation = 2131034112;
@@ -207,10 +213,13 @@ namespace Vrili.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int MainView = 2130903040;
+			public const int item_activity = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int SplashScreen = 2130903041;
+			public const int MainView = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int SplashScreen = 2130903042;
 			
 			static Layout()
 			{
