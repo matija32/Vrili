b0286df1de3224cc326ffdc6748d1a784f5f97ac
Matija Lukic <matija32@gmail.com>
2017-02-19 19:33:43 +0100

    minor renames

--- Vrili/Vrili.Core/App.cs
+++ Vrili/Vrili.Core/App.cs
@@ -11,7 +11,7 @@ namespace Vrili.Core
                 .AsInterfaces()
                 .RegisterAsLazySingleton();
 
-            RegisterAppStart<ViewModels.FirstViewModel>();
+            RegisterAppStart<ViewModels.MainViewModel>();
         }
     }
 }
