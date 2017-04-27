e618c6755f11901b197576c4199f2e47792ba685
Matija Lukic <matija32@gmail.com>
2017-04-27 19:07:33 +0200

    added unit testing project

---
+++ Vrili.Core.Tests/RecipeViewModelTests.cs
@@ -0,0 +1,18 @@
+﻿using MvvmCross.Test.Core;
+using Moq;
+using NUnit.Framework;
+
+namespace Vrili.Core.Tests
+{
+    [TestFixture]
+    public class RecipeViewModelTests : MvxIoCSupportingTest
+    {
+        [Test]
+        public void TestViewModel()
+        {
+            base.Setup(); // from MvxIoCSupportingTest
+            
+            int x = 3 + 2;
+        }
+    }
+}
